ba3d38b51f82c9c78225f05d44b66cc71b3bbe81
Ganesh Katrapati <nesh@skynet>
2015-07-07 12:18:50 +0530

    Random Reads

--- index.c
+++ index.c
@@ -6,16 +6,17 @@
 
 
 const char *argp_program_version =
-  "1.0";
+"1.0";
 const char *argp_program_bug_address =
-  "<ganesh.katrapati@research.iiit.ac.in>";
+"<ganesh.katrapati@research.iiit.ac.in>";
 
 static char doc[] =
-  "A program which computes the index of a given file and provides fast search.";
+"A program which computes the index of a given file and provides fast search.";
 
 static struct argp_option options[] = {
   {"write",   'w', 0,  0, "Compute the index and save it to the file" },
   {"read",   'r', 0,  0, "Read the index file instead of making a new one." },
+  {"random_reads",   'n', "count",  0, "Choose count number of random line numbers to read" },
   {"line-number",   'l', "line_number",  0, "Provide which line number to read" },
   { 0 }
 };
@@ -27,47 +28,52 @@ struct arguments {
   int read;
   int write;
   long line_number;
+  long random_count;
 };
 
 
 
 static error_t parse_opt (int key, char *arg, struct argp_state *state){
   /* Get the input argument from argp_parse, which we
-     know is a pointer to our arguments structure. */
+  know is a pointer to our arguments structure. */
   struct arguments *arguments = state->input;
   //printf("%s\n", arguments -> args[0]);
   switch (key)
-    {
+  {
     case ARGP_KEY_ARG:
-      if (state->arg_num >= 2)
-        /* Too many arguments. */
-        argp_usage (state);
+    if (state->arg_num >= 2)
+    /* Too many arguments. */
+    argp_usage (state);
 
-      arguments->args[state->arg_num] = arg;
+    arguments->args[state->arg_num] = arg;
 
-      break;
+    break;
 
     case ARGP_KEY_END:
-        /* Not enough arguments. */
-      if (state->arg_num < 2)
-        argp_usage (state);
-      break;
+    /* Not enough arguments. */
+    if (state->arg_num < 2)
+    argp_usage (state);
+    break;
 
     case 'r':
-          arguments -> read  = 1;
+    arguments -> read  = 1;
 
-          break;
+    break;
 
     case 'l':
-          arguments -> line_number = atol(arg);
-          break;
+    arguments -> line_number = atol(arg);
+    break;
+
+    case 'n':
+    arguments -> random_count = atol(arg);
+    break;
 
     case 'w': default:
-      //printf("%s\n", arguments -> args[0]);
-      arguments -> write = 1;
-      //make_index_from_file(arguments -> args[0], arguments -> args[1], 0);
+    //printf("%s\n", arguments -> args[0]);
+    arguments -> write = 1;
+    //make_index_from_file(arguments -> args[0], arguments -> args[1], 0);
 
-    }
+  }
   return 0;
 }
 
@@ -84,14 +90,36 @@ int main(int argc, char  ** argv) {
   arguments.read = 0;
   arguments.write = 0;
   arguments.line_number = -2;
+  arguments.random_count = -2;
   argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
   if (arguments.read) {
-    if(arguments.line_number == -2) {
-      printf("Please Provide the line number too\n");
+    FILE *source_file, *index_file;
+    source_file = fopen(arguments.args[0], "r");
+    index_file = fopen(arguments.args[1], "r");
+
+    if(arguments.line_number != -2) {
+      char *buf = read_from_index(source_file, index_file, arguments.line_number);
+      printf("%s", buf);
+      free(buf);
+    }
+    else if (arguments.random_count != -2) {
+      long i;
+      for (i = 0; i < arguments.random_count; i++) {
+        int line_number  = rand() % 10000;
+        char *buf = read_from_index(source_file, index_file, line_number);
+        printf("%s", buf);
+        free(buf);
+      }
+    }
+    else {
+      printf("Please Provide the line number or supply -n for random reads\n");
       exit(-1);
     }
-    read_from_index(arguments.args[0], arguments.args[1], arguments.line_number);
+
+    fclose(source_file);
+    fclose(index_file);
+
   }
   else {
     make_index_from_file(arguments.args[0],arguments.args[1],0);
--- index.h
+++ index.h
@@ -2,76 +2,77 @@
 #include <stdlib.h>
 
 /**
-  A function which takes a source file, loads up its index, goes to the desired line
-  and fetches the line from the source file.
-  @param source_file  The input file
-  @param index_file   The generated index_file
-  @param desired_line The desired line
+A function which takes a source file, loads up its index, goes to the desired line
+and fetches the line from the source file.
+@param source_file  The input file
+@param index_file   The generated index_file
+@param desired_line The desired line
 */
 
-int read_from_index(char *source_file, char *index_file, long desired_line) {
-    FILE * fp, *sp;
-    fp = fopen(index_file, "r");
-    long line_number = 0;
-    long begin;
-    long end;
-    //long desired_line = 10;
-    fseek(fp, desired_line*3*sizeof(line_number), SEEK_SET);
-    //while (!feof(fp)) {
-      fread(&line_number,sizeof(line_number),1,fp);
-      fread(&begin,sizeof(begin),1,fp);
-      fread(&end,sizeof(end),1,fp);
-      printf("%ld %ld %ld\n", line_number, begin, end );
-    //}
+char * read_from_index(FILE *source_file, FILE *index_file, long desired_line) {
+  long line_number = 0;
+  long begin;
+  long end;
 
-    sp = fopen(source_file, "r");
-    fseek(sp, begin, SEEK_SET);
-    char buf[end -begin];
-    buf[0] = '\0';
-    fread(buf, end - begin, 1, sp);
-    printf("%s\n", buf);
-    fclose(fp);
-    fclose(sp);
+  /*
+    For each line in the source file, a tuple of three 32 bit longs are stored
+      -> [line_number, begin, end].
+   */
+  fseek(index_file, desired_line*3*sizeof(line_number), SEEK_SET);
+
+  fread(&line_number,sizeof(line_number),1,index_file);
+  fread(&begin,sizeof(begin),1,index_file);
+  fread(&end,sizeof(end),1,index_file);
+
+
+  // Seek to the begin in the source file.
+  fseek(source_file, begin, SEEK_SET);
+
+  char * buf = malloc(sizeof(char) * (end -begin));
+  buf[0] = '\0';
+  fread(buf, end - begin, 1, source_file);
+  //printf("%s", buf);
+  return buf;
 
 }
 
 /**
- A function which builds an index file for a given source file
- @param source_file The input file
- @param target_file The file to which index is saved
+A function which builds an index file for a given source file
+@param source_file The input file
+@param target_file The file to which index is saved
 */
 int make_index_from_file(char const *source_file, char const *target_file, int visual) {
-    FILE * fp, * op;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    long line_number = 0;
-    long total_size_read = 0;
+  FILE * fp, * op;
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  long line_number = 0;
+  long total_size_read = 0;
 
-    fp = fopen(source_file, "r");
-    remove(target_file);
-    op = fopen(target_file, "a");
+  fp = fopen(source_file, "r");
+  remove(target_file);
+  op = fopen(target_file, "a");
 
-    if (fp == NULL)
-        return -200; /* A Custom error for informing that, your file does not exist*/
+  if (fp == NULL)
+  return -200; /* A Custom error for informing that, your file does not exist*/
 
-    while ((read = getline(&line, &len, fp)) != -1) {
-      //printf("%d %zu %zu\n", line_number, total_size_read, total_size_read + read);
-      fwrite(&line_number, sizeof(line_number), 1, op);
-      fwrite(&total_size_read, sizeof(total_size_read), 1, op);
-      total_size_read += read;
-      fwrite(&total_size_read, sizeof(total_size_read), 1, op);
+  while ((read = getline(&line, &len, fp)) != -1) {
+    //printf("%d %zu %zu\n", line_number, total_size_read, total_size_read + read);
+    fwrite(&line_number, sizeof(line_number), 1, op);
+    fwrite(&total_size_read, sizeof(total_size_read), 1, op);
+    total_size_read += read;
+    fwrite(&total_size_read, sizeof(total_size_read), 1, op);
 
-      // if (visual && ((line_number % 1000) == 0)){
-      //   printf("\r%d", line_number);
-      // }
-      line_number++;
+    // if (visual && ((line_number % 1000) == 0)){
+    //   printf("\r%d", line_number);
+    // }
+    line_number++;
 
-    }
-    fclose(fp);
+  }
+  fclose(fp);
 
-    if (line)
-        free(line);
+  if (line)
+  free(line);
 
-    return 0;
+  return 0;
 }
